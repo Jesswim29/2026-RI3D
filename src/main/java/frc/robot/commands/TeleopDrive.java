4ab32425fa7757c3081c96878063d51acedcd5ff
silversigv <silversi@mail.gvsu.edu>
2025-09-14 18:49:16 -0400

    De-roninify TeleopDrive, move things from RI3d subdirectory to root, help us god

---
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Gyro;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+public class TeleopDrive extends Command {
+
+    /** Creates a new TeleopDrive. */
+    private DoubleSupplier yTranslation;
+    private DoubleSupplier xStrafe;
+    private DoubleSupplier rotation;
+    // The following is seemingly not being used anywhere
+    // TODO: Reimplement gyro zeroing (please for the love of something give this variable a sane name, not m_a)
+    // private BooleanSupplier m_a; // used to store the gyro reset button
+    private Drive m_drive;
+    private Gyro m_gyro;
+
+    public TeleopDrive(
+        DoubleSupplier y,
+        DoubleSupplier x,
+        DoubleSupplier rot,
+        BooleanSupplier a,
+        Drive drive,
+        Gyro gyro
+    ) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        yTranslation = y;
+        xStrafe = x;
+        rotation = rot;
+        m_a = a;
+        m_drive = drive;
+        m_gyro = gyro;
+        addRequirements(m_drive);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        double yVal = MathUtil.applyDeadband(
+            yTranslation.getAsDouble(),
+            Constants.kDeadzone
+        );
+
+        double xVal = MathUtil.applyDeadband(
+            xStrafe.getAsDouble(),
+            Constants.kDeadzone
+        );
+
+        double rotVal = MathUtil.applyDeadband(
+            rotation.getAsDouble(),
+            Constants.kDeadzone
+        );
+
+        // commented: see above
+        //if (m_a.getAsBoolean()) {
+        //    m_gyro.zeroGyro();
+        //}
+
+        m_drive.swerve(
+            new Translation2d(-yVal, xVal).times(DrivetrainConstants.maxSpeed),
+            rotVal * DrivetrainConstants.maxTurningSpeed,
+            false
+        ); // TODO: Get this to work
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
