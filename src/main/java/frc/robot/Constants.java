dae42f83ce7339c7a8267bbe6eb1be5c956ccd67
Josh Howery <joshowery@gmail.com>
2026-01-10 10:29:54 -0500

    Testing javadoc constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.util.Units;
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
+ * @param maxSpeed The maximum linear speed of a wheel
  */
 public final class Constants {
 
