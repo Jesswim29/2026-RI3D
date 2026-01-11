e466fc5d6add4a9b91ea3423d83c45287d22429e
Josh Howery <joshowery@gmail.com>
2026-01-11 17:56:24 -0500

    Adding launcher subsystem java file

---
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.gyros.Gyro;
+import com.revrobotics.*;
+
+
+public class Launcher extends SubsystemBase {
+    private final Spark Fly1 = new Spark(20);    
+
+    
+
+
+
+
+
+}
